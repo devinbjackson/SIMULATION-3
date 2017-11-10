a6a3f6580dc54ef0c32434690980aa13cbeb0b71
Jack <jsjutzi@gmail.com>
2017-11-10 13:49:24 -0600

    second commit

--- sim-3/src/Login.js
+++ sim-3/src/Login.js
@@ -1,31 +1,27 @@
 import React, { Component } from "react";
-
-import './Login.css'
-
+import "./Login.css";
 
 class Login extends Component {
   constructor() {
     super();
 
     this.state = {
-      username: 'abc',
-      password: '123'
+      username: "abc",
+      password: "123"
     };
-
-
   }
 
   render() {
-      console.log(this.state)
+    console.log(this.state);
     return (
-    <div id='background'>
-        <div id='center-block'>
-            <img src=""></img>
-            <div id='helo'>Helo</div>
-            <div id='button'>Login / Register</div>
+      <div id="background">
+        <div id="center-block">
+          <img src={require("./Images/logo.png")} />
+          <div id="helo">Helo</div>
+          <div id="button">Login / Register</div>
         </div>
-    </div>
-    )
+      </div>
+    );
   }
 }
 
